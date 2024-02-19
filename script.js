18a2814753012b9df41ff8e6a63a834471cb2bb1
Fabien <fabien.bonnemayre@gmail.com>
2024-02-19 13:41:57 +0100

    Gestion du json
    Debut de la gestion de la base de données avec la création de 2 fonctions avec template pour voir les artistes favoris et les titres favoris

---
+++ script.js
@@ -0,0 +1,47 @@
+fetch('data.json') // requête vers le fichier JSON
+  .then(response => response.json()) // convertir la réponse textuelle en JSON
+  .then(data => {
+
+// Fonction pour mes titres favoris
+    function mesTitresFavoris(data){
+        // récupération du template
+        let template = document.getElementById('mesTitresFavoris');
+        // parcourir les chansons
+        for (let i = 0; i < 3; i++) {
+            // faire un clone du template
+            const clone = template.content.cloneNode(true);
+            // remplir le clone
+            clone.querySelector('.card-title').textContent = data[i].name;
+            clone.querySelector('.card-text').textContent = data[i].artists[0].name;
+            clone.querySelector('.album').textContent = data[i].album.name
+            clone.querySelector('.card-img').src = data[i].album.images[0].url;
+            clone.querySelector('.card-img').alt = data[i].name;
+    
+            // ajouter le clone au DOM dans le conteneur
+            document.getElementById('trackList').appendChild(clone);
+        }
+    }
+    
+// Fonction pour mes artistes favoris
+    function mesArtistesFavoris(data){
+        let template = document.getElementById('mesArtistesFavoris');
+        for (let i = 0; i < 3; i++) {
+            // faire un clone du template
+            const clone = template.content.cloneNode(true);
+            // remplir le clone
+            clone.querySelector('.card-title').textContent = data[i].artists[0].name;
+            clone.querySelector('.card-img-top').src = data[i].artists[0].images[2].url
+            clone.querySelector('.card-img-top').alt = data[i].artists[0].name;
+    
+            // ajouter le clone au DOM dans le conteneur
+            document.getElementById('trackList2').appendChild(clone);
+        }
+        
+    }
+
+//Appel des fonctions
+    mesTitresFavoris(data);
+    mesArtistesFavoris(data);
+
+});
+
